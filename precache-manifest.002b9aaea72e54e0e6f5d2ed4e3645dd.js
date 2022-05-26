8fb9e8eab1f267e37446c2dddb4df3a5bea68d47
Ayush Gangwar <ayushgangwarrr@gmail.com>
2022-05-27 02:35:36 +0530

    Updates

---
+++ precache-manifest.002b9aaea72e54e0e6f5d2ed4e3645dd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a142fc27a7dcdfcb8ca3",
+    "url": "/textutils/static/css/main.ecc62427.chunk.css"
+  },
+  {
+    "revision": "a142fc27a7dcdfcb8ca3",
+    "url": "/textutils/static/js/main.a142fc27.chunk.js"
+  },
+  {
+    "revision": "7ab2393a9ad8c2b58dcd",
+    "url": "/textutils/static/js/1.7ab2393a.chunk.js"
+  },
+  {
+    "revision": "5b977f20bbc5700a25d3",
+    "url": "/textutils/static/js/2.5b977f20.chunk.js"
+  },
+  {
+    "revision": "1ad6ac821b09f78178e7",
+    "url": "/textutils/static/js/runtime~main.1ad6ac82.js"
+  },
+  {
+    "revision": "95b3102ba28891eb46734ca78dc1e50a",
+    "url": "/textutils/index.html"
+  }
+];
